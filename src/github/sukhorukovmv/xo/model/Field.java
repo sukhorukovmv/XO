3bd6b30229e0603ba4a762b066441b3d5fc2983d
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-02-19 23:03:12 +0300

    Field checks have been added

--- src/github/sukhorukovmv/xo/model/Field.java
+++ src/github/sukhorukovmv/xo/model/Field.java
@@ -1,7 +1,7 @@
 package github.sukhorukovmv.xo.model;
 
 import github.sukhorukovmv.xo.model.exceptions.AlreadyOccupiedException;
-import github.sukhorukovmv.xo.model.exceptions.InvalidePointException;
+import github.sukhorukovmv.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
@@ -16,16 +16,16 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) throws InvalidePointException {
+    public Figure getFigure(final Point point) throws InvalidPointException {
         if (!checkPoint(point)) {
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) throws InvalidePointException, AlreadyOccupiedException {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
         if (!checkPoint(point)) {
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
         if (field[point.x][point.y] != null) {
             throw new AlreadyOccupiedException();
