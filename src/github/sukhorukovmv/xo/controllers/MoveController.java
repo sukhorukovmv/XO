04317c1227f5fd4dba8835c5088d99757c9d650b
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-02-25 10:55:17 +0300

    MoveCotroller - add method applyFigure
    applyFigure set Figure on Field

--- src/github/sukhorukovmv/xo/controllers/MoveController.java
+++ src/github/sukhorukovmv/xo/controllers/MoveController.java
@@ -1,7 +1,16 @@
 package github.sukhorukovmv.xo.controllers;
 
-public class MoveController {
-    public void applyFigure() {
+import github.sukhorukovmv.xo.model.Field;
+import github.sukhorukovmv.xo.model.Figure;
+import github.sukhorukovmv.xo.model.Point;
+import github.sukhorukovmv.xo.model.exceptions.AlreadyOccupiedException;
+import github.sukhorukovmv.xo.model.exceptions.InvalidPointException;
 
+public class MoveController {
+    public void applyFigure(final Field field, final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
     }
 }
