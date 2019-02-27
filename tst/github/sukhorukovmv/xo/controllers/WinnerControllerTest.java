c2136d2d39e43724ffeb6e14ca37f2bfb420d74b
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-02-27 23:07:14 +0300

    WinnerControllerTest - created new tests

--- tst/github/sukhorukovmv/xo/controllers/WinnerControllerTest.java
+++ tst/github/sukhorukovmv/xo/controllers/WinnerControllerTest.java
@@ -3,25 +3,82 @@ package github.sukhorukovmv.xo.controllers;
 import github.sukhorukovmv.xo.model.Field;
 import github.sukhorukovmv.xo.model.Figure;
 import github.sukhorukovmv.xo.model.Point;
+import github.sukhorukovmv.xo.model.exceptions.AlreadyOccupiedException;
 import github.sukhorukovmv.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 
 public class WinnerControllerTest {
 
     @Test
-    public void getWinnerRows() {
+    public void checkWinnerOnRows() throws AlreadyOccupiedException, InvalidPointException {
         final Field field = new Field(3);
-        final WinnerController winner = new WinnerController();
 
-        try {
-            field.setFigure(new Point(0, 0), Figure.O);
-            field.setFigure(new Point(0, 1), Figure.O);
-            field.setFigure(new Point(0, 2), Figure.O);
-            assertEquals(Figure.O, winner.getWinner(field));
-        } catch (InvalidPointException e) {
-            e.printStackTrace();
-        }
+        new MoveController().applyFigure(field, new Point(0, 0), Figure.O);
+        new MoveController().applyFigure(field, new Point(0, 1), Figure.O);
+        new MoveController().applyFigure(field, new Point(0, 2), Figure.O);
+
+        assertEquals(Figure.O, new WinnerController().getWinner(field).get());
+    }
+
+    @Test
+    public void checkWinnerOnMainDiag() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field(3);
+
+        new MoveController().applyFigure(field, new Point(0, 0), Figure.X);
+        new MoveController().applyFigure(field, new Point(1, 1), Figure.X);
+        new MoveController().applyFigure(field, new Point(2, 2), Figure.X);
+
+        assertEquals(Figure.X, new WinnerController().getWinner(field).get());
+    }
+
+    @Test
+    public void checkWinnerOnSideDiag() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field(3);
+
+        new MoveController().applyFigure(field, new Point(0, 2), Figure.X);
+        new MoveController().applyFigure(field, new Point(1, 1), Figure.X);
+        new MoveController().applyFigure(field, new Point(2, 0), Figure.X);
+
+        assertEquals(Figure.X, new WinnerController().getWinner(field).get());
+    }
+
+    @Test
+    public void checkWinnerDraw() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field(3);
+
+        new MoveController().applyFigure(field, new Point(0, 0), Figure.O);
+        new MoveController().applyFigure(field, new Point(0, 1), Figure.X);
+        new MoveController().applyFigure(field, new Point(0, 2), Figure.O);
+        new MoveController().applyFigure(field, new Point(1, 0), Figure.X);
+        new MoveController().applyFigure(field, new Point(1, 1), Figure.X);
+        new MoveController().applyFigure(field, new Point(1, 2), Figure.O);
+        new MoveController().applyFigure(field, new Point(2, 0), Figure.X);
+        new MoveController().applyFigure(field, new Point(2, 1), Figure.O);
+        new MoveController().applyFigure(field, new Point(2, 2), Figure.X);
+
+        Optional<Figure> optionalFigure = new WinnerController().getWinner(field);
+
+        if (optionalFigure.isPresent())
+            fail();
+    }
+
+    @Test
+    public void checkWinner() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field(3);
+
+        new MoveController().applyFigure(field, new Point(0, 2), Figure.X);
+        new MoveController().applyFigure(field, new Point(1, 1), Figure.X);
+        new MoveController().applyFigure(field, new Point(2, 0), Figure.O);
+
+        Optional<Figure> optionalFigure = new WinnerController().getWinner(field);
+
+        if (!optionalFigure.isPresent())
+            fail();
+       // assertEquals(Figure.X, new WinnerController().getWinner(field).get());
+
     }
 }
